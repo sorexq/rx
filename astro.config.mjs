895f4db4aac952cb595cd3c783f15b6b92eab159
_sorex <208826388+sorexq@users.noreply.github.com>
2026-04-29 14:51:26 +0600

    added files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sorexq.com',
 	integrations: [
 		starlight({
 			title: 'My Docs',
+      plugins: [starlightThemeFlexoki()],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
